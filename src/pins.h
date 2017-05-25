e00841ed0e276eec14a05623e3ff76a7bfdd9316
Gabriel Burnworth <gabrielburnworth@users.noreply.github.com>
2017-05-24 23:25:09 -0700

    Farmduino v1.0 pins DRAFT

--- src/pins.h
+++ src/pins.h
@@ -1,63 +1,70 @@
 
-// For RAMPS 1.4
-#define X_STEP_PIN 54
-#define X_DIR_PIN 55
-#define X_ENABLE_PIN 38
-#define X_MIN_PIN 3
-#define X_MAX_PIN 2
+// For Farmduino v1.0
+
+// X-AXIS
+#define X_STEP_PIN 26 // X1_STEP_PIN
+#define X_DIR_PIN 27 // X1_DIR_PIN
+#define X_ENABLE_PIN 25 // X1_ENABLE_PIN
+#define E_STEP_PIN 15 // X2_STEP_PIN
+#define E_DIR_PIN 30 // X2_DIR_PIN
+#define E_ENABLE_PIN 14 // X2_ENABLE_PIN
+#define X_MIN_PIN 15 // ANALOG NUMBERING
+#define X_MAX_PIN 14 // ANALOG NUMBERING
 #define X_ENCDR_A 16
 #define X_ENCDR_B 17
-#define X_ENCDR_A_Q 31
-#define X_ENCDR_B_Q 33
+#define X_ENCDR_A_Q -1 // N/A
+#define X_ENCDR_B_Q -1 // N/A
 
-#define Y_STEP_PIN 60
-#define Y_DIR_PIN 61
-#define Y_ENABLE_PIN 56
-#define Y_MIN_PIN 14
-#define Y_MAX_PIN 15
+// Y-AXIS
+#define Y_STEP_PIN 32
+#define Y_DIR_PIN 33
+#define Y_ENABLE_PIN 31
+#define Y_MIN_PIN 13 // ANALOG NUMBERING
+#define Y_MAX_PIN 12 // ANALOG NUMBERING
 #define Y_ENCDR_A 23
-#define Y_ENCDR_B 25
-#define Y_ENCDR_A_Q 35
-#define Y_ENCDR_B_Q 37
+#define Y_ENCDR_B 24
+#define Y_ENCDR_A_Q -1 // N/A
+#define Y_ENCDR_B_Q -1 // N/A
 
-#define Z_STEP_PIN 46
-#define Z_DIR_PIN 48
-#define Z_ENABLE_PIN 62
-#define Z_MIN_PIN 18
-#define Z_MAX_PIN 19
-#define Z_ENCDR_A 27
-#define Z_ENCDR_B 29
-#define Z_ENCDR_A_Q 39
-#define Z_ENCDR_B_Q 41
+// Z-AXIS
+#define Z_STEP_PIN 35
+#define Z_DIR_PIN 36
+#define Z_ENABLE_PIN 34
+#define Z_MIN_PIN 11 // ANALOG NUMBERING
+#define Z_MAX_PIN 10 // ANALOG NUMBERING
+#define Z_ENCDR_A 29
+#define Z_ENCDR_B 28
+#define Z_ENCDR_A_Q -1 // N/A
+#define Z_ENCDR_B_Q -1 // N/A
 
-#define UTM_C 63
-#define UTM_D 59
-#define UTM_E 40
-#define UTM_F 64
-#define UTM_G 42
-#define UTM_H 44
-#define UTM_I 65
-#define UTM_J 66
-#define UTM_K 52
-#define UTM_L 53
+// UTM
+#define UTM_C 9 // ANALOG NUMBERING - TOOL VERIFICATION
+#define UTM_D 5 // ANALOG NUMBERING - SOIL SENSOR
+#define UTM_E -1
+#define UTM_F -1
+#define UTM_G -1
+#define UTM_H -1
+#define UTM_I -1
+#define UTM_J -1
+#define UTM_K -1
+#define UTM_L -1
+// Available digital pins: 2,3,18,19,22,38,39,42,43,44,45,46,47,48,49
+// Available analog pins: 0,1,2,3,4,6,7,8
 
-#define E_STEP_PIN 26
-#define E_DIR_PIN 28
-#define E_ENABLE_PIN 24
-
-#define SDPOWER -1
-#define SDSS 53
 #define LED_PIN 13
 
-#define FAN_PIN 9
+// Peripherals
+#define VACUUM_PIN 12
+#define WATER_PIN 10
+#define LIGHTING_PIN 9
+#define PERIPHERAL_1_PIN 8
+#define PERIPHERAL_2_PIN 7
 
-#define PS_ON_PIN 12
-#define KILL_PIN -1
-
-#define HEATER_0_PIN 10
-#define HEATER_1_PIN 8
-#define TEMP_0_PIN 13 // ANALOG NUMBERING
-#define TEMP_1_PIN 14 // ANALOG NUMBERING
-
+// Auxiliary motors
+#define AUX_STEP_PIN 40
+#define AUX_DIR_PIN 41
+#define AUX_ENABLE_PIN 37
 #define SERVO_0_PIN 4
 #define SERVO_1_PIN 5
+#define SERVO_2_PIN 6
+#define SERVO_3_PIN 7
